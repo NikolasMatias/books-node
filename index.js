8e7b6d61eb296c9b1f3287802d1c837efcf66fa8
Nikolas Matias <nikolas.matias500@gmail.com>
2022-07-20 02:58:29 -0300

    Cleaning index.js and creating .gitignore in public

--- index.js
+++ index.js
@@ -3,20 +3,3 @@ import Server from './src/routes/Server.js'
 (new Server()).listen(() => {
     console.log('passando aqui');
 });
-
-
-
-
-// import env from "./src/helpers/env.js";
-// import path from 'path';
-//
-// const host = await env('HTTP_HOST', '127.0.0.1');
-// const port = await env('HTTP_PORT', '3000');
-//
-// const filename = path.join(process.cwd(), '/src/routes/Route');
-//
-// console.log(host);
-// console.log(port);
-// console.log(filename);
-//
-// console.log(path.extname(filename).length);
