dd24ee5d18b39ebdef67f84fcd3030603df373ea
Nikolas Matias <nikolas.matias500@gmail.com>
2022-07-20 03:23:08 -0300

    Add Handler Error to Server.js

--- src/routes/Server.js
+++ src/routes/Server.js
@@ -35,28 +35,72 @@ export default class Server {
             (new FileHandler).verifyExistAndReadable(pathName)
                 .then(() => {
                     fs.readFile(pathName, 'binary', (err, file) => {
+                        this.#handleIfHasError(response, err)
+                            .then(() => {
+                                response.writeHead(200, { "Content-Type": this.#contentTypesByExtension[path.extname(pathName)] });
+                                response.write(file, 'binary');
+                                response.end();
+                            });
+                    })
+                })
+                .catch( async () => {
+                    this.#handleNotFound(response);
+                });
+        } else {
+            this.#handleNotFound(response);
+        }
+    }
+
+    async #handleIfHasError(response, error = null) {
+        if (error !== null) {
+            const pathName505 = path.join(process.cwd(), await env('URL_500', '/public/view/500.html'));
+            (new FileHandler).verifyExistAndReadable(pathName505)
+                .then(() => {
+                    fs.readFile(pathName505, 'binary', (err, file) => {
                         if (err !== null) {
                             response.writeHead(500, {"Content-Type": "text/plain"});
                             response.write(err + "\n");
                             response.end();
                         } else {
-                            response.writeHead(200, { "Content-Type": this.#contentTypesByExtension[path.extname(pathName)] });
+                            response.writeHead(500, { "Content-Type": this.#contentTypesByExtension[path.extname(pathName505)] });
                             response.write(file, 'binary');
                             response.end();
                         }
                     })
                 })
-                .catch(({ message, error }) => {
-                    response.writeHead(404, { 'Content-Type': 'text/plain' });
-                    response.write("404 Not Found\n");
+                .catch(() => {
+                    response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write(error + "\n");
                     response.end();
                 });
+
+            throw new Error(error);
         } else {
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.write("404 Not Found\n");
-            response.end();
-            return;
+            return true;
         }
+    }
+
+    async #handleNotFound(response) {
+        const pathName404 = path.join(process.cwd(), await env('URL_404', '/public/view/404.html'));
+        (new FileHandler).verifyExistAndReadable(pathName404)
+            .then(() => {
+                fs.readFile(pathName404, 'binary', (err, file) => {
+                    if (err !== null) {
+                        response.writeHead(500, {"Content-Type": "text/plain"});
+                        response.write(err + "\n");
+                        response.end();
+                    } else {
+                        response.writeHead(404, { "Content-Type": this.#contentTypesByExtension[path.extname(pathName404)] });
+                        response.write(file, 'binary');
+                        response.end();
+                    }
+                })
+            })
+            .catch(() => {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.write("404 Not Found\n");
+                response.end();
+            })
     }
 
     #hasExtension(filename) {
