73e92407eef44817b7f3fe93b2eeb1a1cb878be6
Nikolas Matias <nikolas.matias500@gmail.com>
2022-07-31 21:57:14 -0300

    Clean index.js

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ import http from 'http';
 
 const server = new Server();
 
-http.createServer(function (req, res) {
-
-})
-
 server.concatRoutes(api).concatRoutes(web);
-
-// server.getJson('/teste', () => ({teste: 1}));
 
 server.listen();
